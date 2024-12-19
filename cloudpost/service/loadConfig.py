3abc3229d749404dbf31124427e57d3fdaf63654
EnderRomantice <enderromantic@gmail.com>
2024-12-19 14:00:23 +0800

    Update loadConfig.py

--- cloudpost/service/loadConfig.py
+++ cloudpost/service/loadConfig.py
@@ -2,11 +2,11 @@ import os
 import json
 
 
-current_dir =  os.path.dirname(os.path.dirname(__file__))
-file_path = os.path.join(current_dir, 'config', 'logConfig.json')
+getDir =  os.path.dirname(os.path.dirname(__file__))
+configDir = os.path.join(getDir, 'config', 'logConfig.json')
 
 def readStartConfig(self):
-    with open(file_path, 'r') as f:
+    with open(configDir, 'r') as f:
         start =json.load(f)
 
 
@@ -14,7 +14,7 @@ def readStartConfig(self):
         self.ui.postInput.setText(str(start["data"][-1]["postData"]))
 
 def loadsConfig(url, postdata):
-    with open(file_path, 'r') as f:
+    with open(configDir, 'r') as f:
         start =json.load(f)
         start["data"].append({"url": url, "postData": postdata})
 
